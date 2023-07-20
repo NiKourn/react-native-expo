95b0f0a0e430687be0927d081a2a24f93f4c255b
nkourniotis <nikourn@gmail.com>
2023-07-20 16:47:39 +0100

    changed api call, need more work

--- src/context/nested/ShopContext.tsx
+++ src/context/nested/ShopContext.tsx
@@ -5,10 +5,12 @@ import APIKitDummy from '../../API/APIKit';
 const defaultContext = {
 	isLoading: false,
 	getProducts: () => {},
-	Products: [],
+	pagedProducts: [],
 	loadMore: () => {},
+	getPaginatedProducts: () => {},
 	isAllDataLoaded: false,
 	page: 1,
+	productsArray: [],
 };
 
 export const ShopContext = createContext(defaultContext);
@@ -17,17 +19,20 @@ const ShopProvider = ({ children }) => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [page, setPage] = useState(1);
 	const [totalResults, setTotalResults] = useState(1);
-	const [Products, setProducts] = useState([]);
-    const [isAllDataLoaded, setIsAllDataLoaded] = useState(false);
+	const [productsArray, setProductsArray] = useState([]);
+	const [isAllDataLoaded, setIsAllDataLoaded] = useState(false);
+	const [pagedProducts, setPagedProducts] = useState([]);
 
-	let limit = 15;
-	//get all products listing
+	const paginationLimit = 15;
+
+	// Get all products listing
 	const getProducts = async () => {
 		setIsLoading(true);
 		let getKey = await AsyncStorage.getItem('key');
 
-		const skip = (page - 1) * limit;
-		await APIKitDummy.get(`auth/products/?limit=${limit}&skip=${skip}`, {
+		// const skip = (page - 1) * limit;
+
+		await APIKitDummy.get(`auth/products?limit=0`, {
 			headers: {
 				Authorization: `Bearer ${getKey}`,
 				'Content-Type': 'application/json',
@@ -35,41 +40,62 @@ const ShopProvider = ({ children }) => {
 		})
 			.then((res) => {
 				const { products, total } = res.data;
-
-				setProducts([...Products, ...products]);
+				// Calculate the start and end indexes for the current page
+				console.log(products.length);
+				setProductsArray(products);
 				setTotalResults(total);
 				setIsLoading(false);
-				console.log('inside then')
+				console.log('getProducts');
 			})
 			.catch((e) => {
 				console.log('Error Get Products function' + e);
 			});
 	};
 
-    let allPages = Math.ceil(totalResults / limit);
-    const loadMore = () => {
-		if (page < totalResults) {
-			setPage(page + 1);
-		}
-	};
+	let allPages = Math.ceil(totalResults / paginationLimit);
+	// useEffect(() => {
+	// 	// Call getPaginatedProducts whenever productsArray changes
+	// 	getPaginatedProducts();
+	//   }, [page]); // Include 'page' in the dependency array to handle loadMore
 
-    useEffect(() => {
-        if (page >= allPages + 1) {
+	const getPaginatedProducts = () => {
+		const startIndex = (page - 1) * paginationLimit;
+		const endIndex = page * paginationLimit;
+
+		// Slice the products array for the current page
+		let slicedProducts = productsArray.slice(startIndex, endIndex);
+		setPagedProducts([...slicedProducts]);
+		console.log('getPaginatedProducts');
+		// Check if all pages are loaded
+		if (page >= allPages) {
 			setIsAllDataLoaded(true);
 		} else {
 			setIsAllDataLoaded(false);
 		}
-	}, [page,  totalResults]);
+	};
+
+	const loadMore = () => {
+		// if (isLoading || isAllDataLoaded) {
+		// 	// If data is already loading, return early to prevent multiple calls
+		// 	return;
+		// }
+
+		if (page < allPages) {
+			setPage(page + 1);
+		}
+	};
 
 	return (
 		<ShopContext.Provider
 			value={{
 				isLoading,
 				getProducts,
-				Products,
+				pagedProducts,
+				getPaginatedProducts,
 				loadMore,
 				isAllDataLoaded,
 				page,
+				productsArray,
 			}}>
 			{children}
 		</ShopContext.Provider>
--- src/screens/Products.tsx
+++ src/screens/Products.tsx
@@ -6,61 +6,71 @@ import { styles, images } from '../styles/styles';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Products = () => {
-	const { Products, isLoading, loadMore, isAllDataLoaded, page, getProducts } = useContext(ShopContext);
-	const [areImagesLoaded, setImagesLoaded] = useState(false);
-  
-	useEffect(() => {
-	  getProducts();
-	}, [page]);
+  const { productsArray, pagedProducts, isLoading, loadMore, isAllDataLoaded, page, getProducts, getPaginatedProducts } = useContext(ShopContext);
+  const [areImagesLoaded, setImagesLoaded] = useState(false);
 
-	const handleImageLoad = () => {
-		setImagesLoaded(true);
-		setImagesLoaded(true);
-	};
+  useEffect(() => {
+	getProducts();
+		console.log(productsArray.length);
+	  
+	
+  }, []); // Call 'getProducts' whenever 'page' changes
 
-	const myListEmpty = () => {
-		return (
-			<View style={{ alignItems: 'center' }}>
-				<Spinner visible={isLoading} />
-				<Text style={styles.item}>No data found</Text>
-			</View>
-		);
-	};
+  useEffect(() => {
+	getPaginatedProducts();
 
-	const renderItem = ({ item }) => (
-		<View style={styles.product}>
-			<Image source={{ uri: item.thumbnail }} style={images.images} onLoad={handleImageLoad} />
-			<Image source={{ uri: item.thumbnail }} style={images.images} onLoad={handleImageLoad} />
-			<Text key={item.id}>{item.title}</Text>
-		</View>
-	);
+  }, [page, productsArray]);
 
-	return (
-		<SafeAreaView style={styles.safeArea}>
-			<FlatList
-				columnWrapperStyle={styles.flatList}
-				data={Products}
-				numColumns={4}
-				ListEmptyComponent={myListEmpty}
-				ListHeaderComponent={() => (
-					<Text
-						style={{
-							fontSize: 30,
-							textAlign: 'left',
-							marginTop: 20,
-							marginBottom: 10,
-							fontWeight: 'bold',
-						}}>
-						Products||{Products.length}
-					</Text>
-				)}
-				renderItem={renderItem}
-				onEndReached={loadMore} // Load more when reaching the end of the list
-				onEndReachedThreshold={0.001} // Load more when 50% of the list is scrolled
-				ListFooterComponent={<Spinner visible={!isAllDataLoaded && isLoading} />} // Show spinner at the bottom when loading more
-			/>
-		</SafeAreaView>
-	);
+  const handleImageLoad = () => {
+    setImagesLoaded(true);
+  };
+
+  const myListEmpty = () => {
+    return (
+      <View style={{ alignItems: 'center' }}>
+        <Spinner visible={isLoading} />
+        <Text style={styles.item}>No data found</Text>
+      </View>
+    );
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.product}>
+      <Image source={{ uri: item.thumbnail }} style={images.images} onLoad={handleImageLoad} />
+      <Text key={item.id}>{item.title}</Text>
+    </View>
+  );
+
+
+//   console.log(pagedProducts);
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <FlatList
+        columnWrapperStyle={styles.flatList}
+        data={pagedProducts}
+        numColumns={4}
+        ListEmptyComponent={myListEmpty}
+        ListHeaderComponent={() => (
+          <Text
+            style={{
+              fontSize: 30,
+              textAlign: 'left',
+              marginTop: 20,
+              marginBottom: 10,
+              fontWeight: 'bold',
+            }}
+          >
+            Products||{pagedProducts.length}
+          </Text>
+        )}
+        renderItem={renderItem}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.001}
+        ListFooterComponent={<Spinner visible={!isAllDataLoaded && isLoading} />}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default Products;
